69c35699af96391fe25156dfb127e2ab907bc29d
xiaofeng <469724314@qq.com>
2018-01-14 19:50:01 +0800

    add test9&10&11&dtype_test

---
+++ test10.py
@@ -0,0 +1,93 @@
+#************coding:utf-8****************
+import numpy as np
+
+# numpy.sort()
+# sort()函数返回输入数组的排序副本
+# a 要排序的数组
+# axis 沿着它排序数组的轴，如果没有指定则沿着最后的轴排序
+# kind 默认为'quicksort'(快速排序)'quicksort'(快速排序)	'mergesort'(归并排序)	'heapsort'(堆排序)
+# order 如果数组包含字符串，则是要排序的字符串
+a = np.array([[3,6,2],[5,7,1]])
+print np.sort(a)
+# [[2 3 6]
+#  [1 5 7]] 没有指定轴，因为轴1是最后的轴，则沿轴1进行排序
+print np.sort(a,axis=1)
+# [[2 3 6]
+#  [1 5 7]]
+dt = np.dtype([('name',  'S4'),('age',  int)]) # 自定义数据类型，S4表示长度为4的字符串
+a = np.array([("raju",21),("anil",25),("ravi",  17),  ("amar",27)], dtype = dt)
+print np.sort(a, order =  'name')
+# [('amar', 27) ('anil', 25) ('raju', 21) ('ravi', 17)]
+
+# numpy.argsort()
+# 函数对输入数组沿给定轴执行排序，返回反需后原数组的索引
+a = np.array([4,  1,  3])
+print np.argsort(a) #[1 2 0]
+a = np.array([[[14,5,2],[7,13,4]],[[11,2,9],[1,8,12]]])
+print np.argsort(a)
+# [[[2 1 0]
+#   [2 0 1]]
+#
+#  [[1 2 0]
+#   [0 1 2]]] 没有指定轴，默认沿轴2进行排序
+print np.argsort(a,2)
+# [[[2 1 0]
+#   [2 0 1]]
+#
+#  [[1 2 0]
+#   [0 1 2]]]
+print np.argsort(a,0)
+# [[[1 1 0]
+#   [1 1 0]]
+#
+#  [[0 0 1]
+#   [0 0 1]]] 沿0轴进行排序
+
+
+# numpy.argmax() 返回指定轴的最大值的索引
+# numpy.argmin() 返回指定轴的最小值的索引
+# 如果没有指定轴，则将数组展开，找最大或最小值，返回索引
+a = np.array([[[14,5,2],[7,13,4]],[[11,2,9],[3,8,15]]])
+print np.argmax(a) #11 没有指定轴，则展开数组
+print np.argmin(a) #2
+print np.argmax(a,0)
+# [[0 0 1]
+#  [0 0 1]]
+print np.argmin(a,0)
+# [[1 1 0]
+#  [1 1 0]]
+print np.argmax(a,2)
+# [[0 1]
+#  [0 2]]
+print np.argmin(a,2)
+# [[2 2]
+#  [1 0]] 沿2轴比较，返回最小值索引
+
+# numpy.nonzero()
+# 函数返回输入数组中非零元素的索引。
+a = np.array([[[0,5,2],[7,13,1]],[[1,2,9],[1,1,15]]])
+print np.nonzero(a)
+# array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1]),
+# array([0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]),
+# array([1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2])
+#返回值从上到下分别代表轴0、轴1、轴2上的索引
+
+# numpy.where()
+# 返回输入数组中满足给定条件的元素的索引
+a = np.array([[[0,5,2],[7,13,1]],[[1,2,9],[1,1,15]]])
+print np.where(a == 0) #(array([0]), array([0]), array([0]))
+print np.where(a>10) #(array([0, 1]), array([1, 1]), array([1, 2]))
+
+# numpy.extract()
+# 函数返回满足任何条件的元素
+a = np.array([[[0,5,2],[7,13,1]],[[1,2,9],[1,1,15]]])
+condtion = np.mod(a,2) == 0
+print condtion #定义条件，condtion只是一个普通的变量，可以随便定义。
+# [[[ True False  True]
+#   [False False False]]
+#
+#  [[False  True False]
+#   [False False False]]]
+print np.extract(condtion, a)
+# [0 2 2] 返回满足条件的元素
+
--- test9.py
+++ test9.py
@@ -91,3 +91,57 @@ print np.median(a,axis = 0)
 print np.median(a,axis = 1)
 # [  1.5   5.5   9.5  13.5]
 
+# numpy.mean()
+# 算术平均值是沿轴的元素的总和除以元素的数量。 numpy.mean()函数返回数组中元素的算术平均值
+a = np.array([[[1,1,1],[2,2,2]],[[3,3,3],[4,4,4]]])
+print a
+# [[[1 1 1]
+#   [2 2 2]]
+#
+#  [[3 3 3]
+#   [4 4 4]]]
+print np.mean(a) #2.5 没有指定轴，把数组展开计算平均值
+print np.mean(a,0) #沿轴0计算平均值 即（a[axis,x1,y1]+a[axis,x2,y2]）/2  x1=x2 y1=y2这里总共有6个
+# [[ 2.  2.  2.]
+#  [ 3.  3.  3.]]
+print np.mean(a,1) #沿轴1计算平均值 即（a[x1,axis,y1]+a[x2,axis,y2]）/2  x1=x2 y1=y2这里总共有6个
+# [[ 1.5  1.5  1.5]
+#  [ 3.5  3.5  3.5]]
+print np.mean(a,2) #沿轴2计算平均值 即（a[x1,y1,axis]+a[x2,y2,axis]）/2  x1=x2 y1=y2这里总共有4个
+# [[ 1.  2.]
+#  [ 3.  4.]]
+
+# numpy.average()
+# 加权平均值是由每个分量乘以反映其重要性的因子得到的平均值。
+# numpy.average()函数根据在另一个数组中给出的各自的权重计算数组中元素的加权平均值。
+# 该函数可以接受一个轴参数。 如果没有指定轴，则数组会被展开。
+# 加权平均值 = (x1*w1+x2*w2+x3*w3+...+xk*wk)/(w1+w2+w3+...+w4)
+# 不指定权重时相当于 mean 函数
+# 如果 returned 参数设为 true，则返回权重的和
+#权重的形状要和数组的形状相同，如果指定了轴，则权重的形状可以不和数组的形状相同，可以只和轴的形状相同
+a = np.arange(6).reshape(3,2)
+wt = np.array([[3,5],[3,4],[3,5]])
+print np.average(a) # 2.5平均值
+print np.average(a, weights = wt)
+# 2.60869565217 没有指定轴
+print np.average(a, axis = 1, weights = wt, returned =  True)
+# (array([ 0.625     ,  2.57142857,  4.625     ]), array([ 8.,  7.,  8.]))
+# 指定了轴1,计算的时候轴1的坐标在变，其他轴的坐标不变，返回权重之和
+wt = np.array([2,2])
+print np.average(a, axis = 1, weights = wt, returned =  True)
+# (array([ 0.5,  2.5,  4.5]), array([ 4.,  4.,  4.]))
+
+# 标准差
+# 标准差是与均值的偏差的平方的平均值的平方根
+# numpy.std
+a = np.arange(4)
+print np.std(a) #1.11803398875
+
+# 方差
+# 标准差是方差的平方根
+# numpy.var
+a = np.arange(4)
+print np.var(a)
+# 1.25
+
+
